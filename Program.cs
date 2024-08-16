dc6e75bfdf196f9c9a25a4054470618ce5815311
Victor Sanchez <vhsv90@gmail.com>
2024-08-16 11:57:56 -0600

    Net Core sln and ExpressionEvaluator library test added

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using CodingSeb.ExpressionEvaluator;
+
+namespace NMLs;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("DotNet Core Console App using some Math Libraries");
+
+        ExpressionEvaluator evaluator = new ExpressionEvaluator();
+        
+        // string expression = """
+        // 1+1
+        // """;
+        // Console.WriteLine(evaluator.Evaluate(expression));
+
+        var continueEvaluating = false;
+        do
+        {
+
+            
+            Console.WriteLine("Type/Copy expression to evaluate");
+            var expressionFromConsole = Console.ReadLine();
+            
+            Console.WriteLine(evaluator.Evaluate(expressionFromConsole) + "\n");
+
+            Console.WriteLine("If you want continue evaluating expression insert Y if not insert N to exit");
+            
+            continueEvaluating = Console.ReadLine().ToUpper() == "Y" ? true: false;
+
+            
+        } while (continueEvaluating);
+
+
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # NMLs
 .Net Math Libraries
+
+
+
+Steps to create this project without cloning the repository
+
+-- using terminal run the command to create the project
+    -- dotnet new console --framework net8.0 --use-program-main
+
+-- run in the terminal the command
+    -- dotnet run
+
+-- to build sln
+    -- dotnet build
+
+
+Github project that we are using for testing
+
+ExpressionEvaluator
+https://github.com/codingseb/ExpressionEvaluator
+
+WIP
